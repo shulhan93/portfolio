afdfe24bb13f8d953e74349c80c7b372837e3df1
Shulhan Andrei <shulhan.andrei@gmail.com>
2019-12-30 19:12:39 +0300

    ready

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,25 @@
 var scene = document.getElementById('scene');
 var parallaxInstance = new Parallax(scene);
 console.log('dsfdasd')
+new WOW().init();
+ var lang = {
+    "html": "90%",
+    "css": "80%",
+    "javascript": "70%",
+    "php": "55%",
+    "angular": "65%"
+  };
+  
+  var multiply = 4;
+  
+  $.each( lang, function( language, pourcent) {
+  
+    var delay = 500;
+    
+    setTimeout(function() {
+      $('#'+language+'-pourcent').html(pourcent);
+    },delay*multiply);
+    
+    multiply++;
+  
+  }); 
