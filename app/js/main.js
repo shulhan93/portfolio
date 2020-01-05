da29a9a7c5df687f5fa9300ea594fea9d0fed8fb
Shulhan Andrei <shulhan.andrei@gmail.com>
2020-01-05 22:52:51 +0300

    add adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,4 @@
+$(function(){
 var scene = document.getElementById('scene');
 var parallaxInstance = new Parallax(scene);
 console.log('dsfdasd')
@@ -9,17 +10,28 @@ new WOW().init();
     "php": "55%",
     "angular": "65%"
   };
-  
+
   var multiply = 4;
-  
+
   $.each( lang, function( language, pourcent) {
-  
+
     var delay = 500;
-    
+
     setTimeout(function() {
       $('#'+language+'-pourcent').html(pourcent);
     },delay*multiply);
-    
+
     multiply++;
-  
-  }); 
+
+  });
+
+  $('.burger-menu').on('click', function() {
+    $('.header__menu').slideToggle(300, function(){
+         if( $(this).css('display') === "none"){
+             $(this).removeAttr('style');
+         }
+    });
+
+ });
+
+});
