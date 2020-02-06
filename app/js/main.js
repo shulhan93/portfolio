7cfad034f3ad39855d9efb933dd9a04d6a1c9a4d
Shulhan Andrei <shulhan.andrei@gmail.com>
2020-02-06 18:49:52 +0300

    add section skill(circle)

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,8 @@
 $(function(){
 var scene = document.getElementById('scene');
+var scene2 = document.getElementById('scene2');
 var parallaxInstance = new Parallax(scene);
-console.log('dsfdasd')
+var parallaxInstance = new Parallax(scene2);
 new WOW().init();
  var lang = {
     "html": "90%",
@@ -33,5 +34,53 @@ new WOW().init();
     });
 
  });
+
+ $('.header .services').hover(
+   function() {
+    $(this).find('.services__img img:first-child').css('display', 'none')
+    $(this).find('.services__img img:last-child').css('display', 'block')
+
+   },
+   function() {
+    $(this).find('.services__img img:first-child').css('display', 'block')
+    $(this).find('.services__img img:last-child').css('display', 'none')
+   }
+ )
+
+ var off = $(".skill__wrapper").offset();
+
+
+
+$('.skill__wrapper').scroll(function() {
+ alert('asdasdasd')
+})
+
+
+
+
+ new CircleProgress('.progress', {
+	max: 100,
+	value: 60,
+  textFormat: 'percent',
+  animationDuration: 2000
+});
+
+new CircleProgress('.progress2', {
+	max: 100,
+	value: 35,
+	textFormat: 'percent',
+});
+
+new CircleProgress('.progress3', {
+	max: 100,
+	value: 75,
+	textFormat: 'percent',
+});
+
+new CircleProgress('.progress4', {
+	max: 100,
+	value: 40,
+	textFormat: 'percent',
+});
 
 });
